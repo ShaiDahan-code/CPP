ba2830c25d588766550d803c3f55592fb2669f4d
Shai Dahan <73953902+ShaiDahan-code@users.noreply.github.com>
2021-07-08 20:59:34 +0300

    Add files via upload

---
+++ Home Work/HW5_Frequency_calculator/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include "Counter.h"
+#include <fstream>
+#include "Parser.h"
+using namespace std;
+int main() {
+    Parser p;
+    p.menu();
+
+    return 0;
+}
